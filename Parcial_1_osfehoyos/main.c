56f7003b50747f38e8713360f305c763ac270d56
Viperexz <osferhoc54@gmail.com>
2022-05-24 21:24:03 -0500

    Primera edicio

---
+++ Parcial_1_osfehoyos/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char * argv[])
+{
+    exit(EXIT_SUCCESS);
+}
---
+++ Parcial_1_osfehoyos/test/hash.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char * obtener_hash(char * nombre_archivo);
+
+int main(int argc, char * argv[])
+{
+    exit(EXIT_SUCCESS);
+}
+
+char * obtener_hash(char * nombre_archivo)
+{
+    char * hash = NULL;
+    return hash;
+}
